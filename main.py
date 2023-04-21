dd449f3a88936697b7796d8500abde6fefa9e68b
RESET0 <799070248@qq.com>
2023-04-21 11:18:28 +0800

    aa

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import torch
+import torch.nn as nn
 m=torch.jit.load('/media/mxc/zgy/IPIN_2022/GCNv2_SLAM/GCN2/gcn2_640x480.pt')
 m.save("/media/mxc/zgy/IPIN_2022/GCNv2_SLAM/GCN2/gcn2_640x480_reload.pt")
